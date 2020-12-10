f546c2dc25e2ae0200ffcd34ea27cf60fdf1be20
Simo Aleksandrov <simo3003@me.com>
2020-12-10 17:25:53 +0200

    Get processes using go-ps

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.15
 require (
 	github.com/AllenDang/giu v0.4.2
 	github.com/JamesHovious/w32 v1.1.0
-	github.com/gen2brain/raylib-go v0.0.0-20201123133337-d123299701ae
+	github.com/mitchellh/go-ps v1.0.0
 )
--- main.go
+++ main.go
@@ -1,64 +1,41 @@
 package main
 
 import (
+	"sort"
 	"strconv"
-	"syscall"
-	"unsafe"
 
 	g "github.com/AllenDang/giu"
-	"github.com/JamesHovious/w32"
+	"github.com/mitchellh/go-ps"
 )
 
-func getProcesses() []string {
-	v := []string{}
-	ps := make([]uint32, 255)
-	var read uint32
-	if !w32.EnumProcesses(ps, 4*uint32(len(ps)), &read) {
-		panic("couldn't read")
-		return v
-	}
+func processesNames(ps []ps.Process) []string {
+	s := []string{}
 	for _, p := range ps {
-		g := getProcessName(p)
-		if g != "" {
-			v = append(v, g)
-		}
+		s = append(s, p.Executable())
 	}
-	return v
+	return s
 }
 
-func getModuleInfo(me32 *w32.MODULEENTRY32) string {
-	procName := syscall.UTF16ToString(me32.SzModule[:])
-	return procName
-}
+func loop(ps []ps.Process) {
+	p := processesNames(ps)
+	sort.Strings(p)
 
-func getProcessName(pid uint32) string {
-	snap := w32.CreateToolhelp32Snapshot(w32.TH32CS_SNAPMODULE, pid)
-	if snap == 0 {
-		return ""
-	}
-	defer w32.CloseHandle(snap)
-
-	var me32 w32.MODULEENTRY32
-	me32.Size = uint32(unsafe.Sizeof(me32))
-	if !w32.Module32First(snap, &me32) {
-		return ""
-	}
-	return getModuleInfo(&me32)
-}
-
-func loop(ps []string) {
 	g.SingleWindow("hello world", g.Layout{
 		g.Line(
-			g.ListBoxV("pids", 250, 500-20, true, ps, nil, nil, nil, nil),
+			g.ListBoxV("pids", 250, 500-20, true, p, nil, nil, nil, nil),
 			g.Label(strconv.Itoa(len(ps))),
 		),
 	})
 }
 
 func main() {
-	ps := getProcesses()
+	processes, err := ps.Processes()
+	if err != nil {
+		panic(err)
+	}
+
 	wnd := g.NewMasterWindow("Quick Kill", 500, 500, g.MasterWindowFlagsNotResizable, nil)
 	wnd.Main(func() {
-		loop(ps)
+		loop(processes)
 	})
 }

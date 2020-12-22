082fd91e7a04fe853143b11dd44e954c67b41880
Simo Aleksandrov <simo3003@me.com>
2020-12-22 19:49:44 +0200

    GROUP PROCESSES

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/AllenDang/giu v0.4.2
 	github.com/JamesHovious/w32 v1.1.0
+	github.com/davecgh/go-spew v1.1.1
 	github.com/mitchellh/go-ps v1.0.0
 )
--- main.go
+++ main.go
@@ -1,66 +1,136 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"sort"
+	"strings"
 
 	g "github.com/AllenDang/giu"
-	"github.com/JamesHovious/w32"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/mitchellh/go-ps"
 )
 
-func processesNames(ps []ps.Process) []string {
-	s := []string{}
-	for _, p := range ps {
-		s = append(s, p.Executable())
-	}
-	return s
+// func FindProcess(ps []Process, pid int) Process {
+// 	for _, p := range ps {
+// 		if p.Pid == pid {
+// 			return p
+// 		}
+// 	}
+// 	return Process{}
+// }
+
+type Process struct {
+	Pid      int
+	PPid     int
+	Cmd      string
+	Children []*Process
 }
 
 type App struct {
-	Processes     []ps.Process
-	SelectedIndex int
+	processes       []*Process
+	pidMap          map[int]*Process
+	SelectedProcess Process
+	FilterWord      string
+}
+
+func (a *App) Processes() error {
+	processes, err := ps.Processes()
+	if err != nil {
+		log.Println("cannot get processes: " + err.Error())
+		return err
+	}
+
+	pids := map[int]*Process{}
+	for _, proc := range processes {
+		// skip pid 0
+		if proc.Pid() == 0 {
+			continue
+		}
+
+		pids[proc.Pid()] = &Process{
+			Pid:  proc.Pid(),
+			PPid: proc.PPid(),
+			Cmd:  proc.Executable(),
+		}
+	}
+
+	for _, p := range processes {
+		if p.Pid() == p.PPid() {
+			continue
+		}
+
+		if proc, ok := pids[p.PPid()]; ok {
+			proc.Children = append(proc.Children, pids[p.Pid()])
+		}
+	}
+
+	a.processes = []*Process{}
+	for _, proc := range pids {
+		if strings.Index(proc.Cmd, a.FilterWord) == -1 {
+			continue
+		}
+
+		a.processes = append(a.processes, proc)
+	}
+
+	sort.Slice(a.processes, func(i, j int) bool {
+		return a.processes[i].Cmd < a.processes[j].Cmd
+	})
+	a.pidMap = pids
+
+	return nil
+}
+
+func (a *App) ProcessRows() []g.Widget {
+	v := []g.Widget{}
+	for _, p := range a.processes {
+		if parent, ok := a.pidMap[p.PPid]; ok && parent.Cmd != p.Cmd {
+			v = append(v, g.Line(ProcessWidget(p)))
+		}
+	}
+	return v
+}
+
+func ProcessWidget(p *Process) g.Widget {
+	if len(p.Children) == 0 {
+		return g.TreeNode(p.Cmd, g.TreeNodeFlagsLeaf, nil)
+	}
+
+	children := []g.Widget{}
+	for _, c := range p.Children {
+		children = append(children, ProcessWidget(c))
+	}
+	// children = append(children, g.Button(fmt.Sprintf("Kill-%d", p.Pid), func() {
+	// 	proc, err := os.FindProcess(p.Pid)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		return
+	// 	}
+
+	// 	if err := proc.Kill(); err != nil {
+	// 		log.Println("Failed killing process")
+	// 	}
+	// }))
+
+	return g.TreeNode(p.Cmd, g.TreeNodeFlagsNone, children)
 }
 
 func (a *App) Loop() {
-	p := processesNames(a.Processes)
-	g.SingleWindow("Quick Kill!", g.Layout{
-		g.Line(
-			g.ListBoxV("pids", 150, 300-15, true, p, nil, func(selectedIndex int) {
-				a.SelectedIndex = selectedIndex
-			}, nil, nil),
-			g.Group(g.Layout{
-				g.LabelWrapped(fmt.Sprintf("%d Procesesses found", len(p))),
-				g.LabelWrapped(fmt.Sprintf("Selected PID: %d - %s", a.SelectedIndex, p[a.SelectedIndex])),
-				g.Button("kill", func() {
-					pid := a.Processes[a.SelectedIndex].Pid()
-					handle, err := w32.OpenProcess(w32.SYNCHRONIZE|w32.PROCESS_TERMINATE, true, uint32(pid))
-					if err != nil {
-						log.Println(err)
-						return
-					}
-					if !w32.TerminateProcess(handle, 0) {
-						log.Println("Failed terminating.")
-						return
-					}
-				}),
-			}),
-		),
-	})
+	g.SingleWindow("Quick Kill!", a.ProcessRows())
 }
 
 func main() {
-	processes, err := ps.Processes()
-	if err != nil {
-		panic(err)
-	}
+	a := App{}
 
-	a := App{Processes: processes}
-	sort.SliceStable(a.Processes, func(i, j int) bool {
-		return a.Processes[i].Executable() < a.Processes[j].Executable()
-	})
+	a.Processes()
+	// go func() {
+	// 	for range time.Tick(time.Second) {
+	// 		a.Processes()
+	// 	}
+	// }()
 
-	wnd := g.NewMasterWindow("Quick Kill", 300, 300, g.MasterWindowFlagsNotResizable, nil)
+	spew.Dump(a.pidMap[13680])
+
+	wnd := g.NewMasterWindow("Quick Kill", 500, 500, g.MasterWindowFlagsNotResizable, nil)
 	wnd.Main(a.Loop)
 }

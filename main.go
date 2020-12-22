866cc79abb3fe6a68f5e67243c7495e716a066bb
Simo Aleksandrov <simo3003@me.com>
2020-12-22 21:07:42 +0200

    Render tree

--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 	"sort"
 	"strings"
+	"time"
 
 	g "github.com/AllenDang/giu"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/mitchellh/go-ps"
 )
-
-// func FindProcess(ps []Process, pid int) Process {
-// 	for _, p := range ps {
-// 		if p.Pid == pid {
-// 			return p
-// 		}
-// 	}
-// 	return Process{}
-// }
 
 type Process struct {
 	Pid      int
@@ -29,8 +22,8 @@ type Process struct {
 type App struct {
 	processes       []Process
 	pidMap          map[int]*Process
-	SelectedProcess Process
-	FilterWord      string
+	filterWord      string
+	selectedProcess Process
 }
 
 func (a *App) Processes() error {
@@ -66,7 +59,7 @@ func (a *App) Processes() error {
 
 	a.processes = []Process{}
 	for _, proc := range pids {
-		if strings.Index(proc.Cmd, a.FilterWord) == -1 {
+		if strings.Index(proc.Cmd, a.filterWord) == -1 {
 			continue
 		}
 
@@ -85,52 +78,70 @@ func (a *App) ProcessRows() []g.Widget {
 	v := []g.Widget{}
 	for _, p := range a.processes {
 		if parent, ok := a.pidMap[p.PPid]; ok && parent.Cmd != p.Cmd {
-			v = append(v, g.Line(ProcessWidget(p)))
+			v = append(v, g.Line(a.ProcessWidget(p)), g.Separator())
 		}
 	}
 	return v
 }
 
-func ProcessWidget(p Process) g.Widget {
+func (a *App) ProcessWidget(p Process) g.Widget {
 	if len(p.Children) == 0 {
-		return g.TreeNode(p.Cmd, g.TreeNodeFlagsLeaf, nil)
+		return g.Line(
+			g.TreeNode(p.Cmd, g.TreeNodeFlagsLeaf, nil),
+			g.SmallButton(fmt.Sprintf("Select %d", p.Pid), func() {
+				a.selectedProcess = p
+			}),
+		)
+
 	}
 
 	children := []g.Widget{}
 	for _, c := range p.Children {
-		children = append(children, ProcessWidget(*c))
+		children = append(children, a.ProcessWidget(*c))
 	}
-	// children = append(children, g.Button(fmt.Sprintf("Kill-%d", p.Pid), func() {
-	// 	proc, err := os.FindProcess(p.Pid)
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		return
-	// 	}
-
-	// 	if err := proc.Kill(); err != nil {
-	// 		log.Println("Failed killing process")
-	// 	}
-	// }))
+	children = append(children,
+		g.SmallButton(fmt.Sprintf("Select %d", p.Pid), func() {
+			a.selectedProcess = p
+		}),
+	)
 
 	return g.TreeNode(p.Cmd, g.TreeNodeFlagsNone, children)
+
 }
 
 func (a *App) Loop() {
-	g.SingleWindow("Quick Kill!", a.ProcessRows())
+	g.SingleWindow("Quick Kill!", g.Layout{
+		g.SplitLayout("Split", g.DirectionHorizontal, true, 300, g.Layout(a.ProcessRows()),
+			g.Group(g.Layout{
+				g.Label(fmt.Sprintf("Selected Process %s - %d", a.selectedProcess.Cmd, a.selectedProcess.Pid)),
+				g.Button(fmt.Sprintf("Kill %d", a.selectedProcess.Pid), func() {
+					proc, err := os.FindProcess(a.selectedProcess.Pid)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+
+					if err := proc.Kill(); err != nil {
+						log.Println("Failed killing process")
+					}
+				}),
+			})),
+	})
 }
 
 func main() {
 	a := App{}
 
 	a.Processes()
-	// go func() {
-	// 	for range time.Tick(time.Second) {
-	// 		a.Processes()
-	// 	}
-	// }()
+	go func() {
+		ticker := time.NewTicker(time.Second * 1)
+		for {
+			a.Processes()
+			g.Update()
+			<-ticker.C
+		}
+	}()
 
-	spew.Dump(a.pidMap[13680])
-
-	wnd := g.NewMasterWindow("Quick Kill", 500, 500, g.MasterWindowFlagsNotResizable, nil)
+	wnd := g.NewMasterWindow("Quick Kill", 500, 500, 0, nil)
 	wnd.Main(a.Loop)
 }
